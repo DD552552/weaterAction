b7307dbbe04c63b61f0a94cbde0ca80338c82547
DD552552 <71481043+DD552552@users.noreply.github.com>
2021-05-08 11:40:33 +0800

    Update herWeater.py

--- herWeater.py
+++ herWeater.py
@@ -57,7 +57,7 @@ def main():
             ganmao = d["data"]["ganmao"] #感冒指数
             tips = d["data"]["forecast"][0]["notice"] #温馨提示
             # 天气提示内容
-            tdwt = "【今日份天气】\n城市:" + parent + city + \
+            tdwt = "【今赣州天气】\n城市:" + parent + city + \
                    "\n日期:" + date + "\n星期:" + week + "\n天气:" + weather_type + "\n温度:" + wendu_high + " / "+ wendu_low + "\n湿度: " + \
                     shidu + "\n空气质量:" + pm25 +"\nPM10: " + pm10 + "\n空气质量: " + quality + \
                    "\n风力风向: " + fx + fl + "\n感冒指数: "  + ganmao + "\n温馨提示:" + tips + "\n更新时间:                       " + update_time +                          "\n✁-----------------------------------------\n" + get_iciba_everyday()
